5708994f65af520ece01cd1765a27e8bc1179d3d
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2022-05-31 21:12:38 +0200

    Singleton Pattern - test

--- DesignPatterns/CreationalPatterns/Singleton/SingletonTest.php
+++ DesignPatterns/CreationalPatterns/Singleton/SingletonTest.php
@@ -14,4 +14,30 @@ class SingletonTest extends TestCase
         $preference = Preference::getInstance();
         $this->assertTrue($preference instanceof Preference);
     }
+
+    /**
+     * @return void
+     */
+    public function testSetData()
+    {
+        $preference = Preference::getInstance();
+        $preference->setProperty('name', 'John');
+        $this->assertEquals('John', $preference->getProperty('name'));
+    }
+
+    /**
+     * @return void
+     */
+    public function testReadOnlyData()
+    {
+        $preference = Preference::getInstance();
+        $this->assertEquals('John', $preference->getProperty('name'));
+    }
+
+    public function testOverrideData()
+    {
+        $preference = Preference::getInstance();
+        $preference->setProperty('name', 'John 2');
+        $this->assertEquals('John 2', $preference->getProperty('name'));
+    }
 }
