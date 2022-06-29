b571c2fccd8942b186ffdf4892741932cd9c4558
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2022-06-29 08:08:09 +0200

    Memento Design Pattern - Ticket

---
+++ DesignPatterns/BehavioralPatterns/Memento/Ticket.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace BehavioralPatterns\Memento;
+
+
+/**
+ * Ticket is the "Originator" in this implementation
+ */
+class Ticket
+{
+    /**
+     * @var State
+     */
+    private State $currentState;
+
+    /**
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->currentState = new State(State::STATE_CREATED);
+    }
+
+    /**
+     * @return void
+     */
+    public function open(): void
+    {
+        $this->currentState = new State(State::STATE_OPENED);
+    }
+
+    /**
+     * @return void
+     */
+    public function assigned(): void
+    {
+        $this->currentState = new State(State::STATE_ASSIGNED);
+    }
+
+    /**
+     * @return void
+     */
+    public function close(): void
+    {
+        $this->currentState = new State(State::STATE_CLOSED);
+    }
+
+    /**
+     * @return Memento
+     */
+    public function saveToMemento(): Memento
+    {
+        return new Memento(clone $this->currentState);
+    }
+
+    /**
+     * @param Memento $memento
+     * @return void
+     */
+    public function restoreFromMemento(Memento $memento)
+    {
+        $this->currentState = $memento->getState();
+    }
+
+    /**
+     * @return State
+     */
+    public function getState(): State
+    {
+        return $this->currentState;
+    }
+}
