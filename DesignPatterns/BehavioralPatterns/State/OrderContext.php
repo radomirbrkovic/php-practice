a78ebd87b3e4b66719e182ed8d11d902a69c9a32
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2022-07-02 10:02:40 +0200

    State Design Pattern - OrderContext

--- DesignPatterns/BehavioralPatterns/State/OrderContext.php
+++ DesignPatterns/BehavioralPatterns/State/OrderContext.php
@@ -11,8 +11,39 @@ class OrderContext
      */
     private StateInterface $state;
 
+    /**
+     * @return OrderContext
+     */
     public static function create(): OrderContext
     {
+        $order = new self();
+        $order->state = new StateCreated();
 
+        return $order;
+    }
+
+    /**
+     * @param StateInterface $state
+     * @return void
+     */
+    public function setState(StateInterface $state)
+    {
+        $this->state = $state;
+    }
+
+    /**
+     * @return void
+     */
+    public function proceedToNext()
+    {
+        $this->state->proceedToNext($this);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function toString()
+    {
+        return $this->state->toString();
     }
 }
