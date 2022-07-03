5ebcc5bcf118b7f449ccad6a5c78dd2d66428da5
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2022-07-03 09:24:13 +0200

    Visitor Design Pattern - RoleVisitorInterface

--- DesignPatterns/BehavioralPatterns/Visitor/Interfaces/RoleVisitorInterface.php
+++ DesignPatterns/BehavioralPatterns/Visitor/Interfaces/RoleVisitorInterface.php
@@ -2,7 +2,20 @@
 
 namespace BehavioralPatterns\Visitor\Interfaces;
 
+use BehavioralPatterns\Visitor\Group;
+use BehavioralPatterns\Visitor\User;
+
 interface RoleVisitorInterface
 {
+    /**
+     * @param User $role
+     * @return mixed
+     */
+    public function visitUser(User $role);
 
+    /**
+     * @param Group $role
+     * @return mixed
+     */
+    public function visitGroup(Group $role);
 }
